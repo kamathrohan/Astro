b5af79c810b2bc84d9e25d06c0a3408731cf806a
Unknown <rohan.kamath9@gmail.com>
2019-03-08 10:02:17 +0000

    Added some plots

--- astroworld.py
+++ astroworld.py
@@ -8,11 +8,19 @@ import matplotlib.pyplot as plt
 hdulist = fits.open("A1_mosaic.fits")
 
 image = hdulist[0].data
-image_info = hdulist.info()
+hist = np.sum(image, axis = 0)
+print(np.argmax(hist))
+plt.plot(hist)
+plt.show()
+#image_info = hdulist.info()
+
+"""
+
 img = cv2.imread("galaxies.jpeg")
 
 print(img)
 kernel = (1/25)*np.array([[0,0,0,0,0],[0,0,0,0,0],[5,5,5,5,5],[0,0,0,0,0],[0,0,0,0,0]])
+dst = cv2.GaussianBlur(img, (5,5),0)
 dst = cv2.filter2D(img,-1,kernel)
 
 
@@ -21,7 +29,8 @@ edges = cv2.Canny(dst,0,5)
 
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edgecks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
-plt.show()s,cmap = 'gray')
-plt.title('Edge Image'), plt.xti
+plt.show()
+"""
