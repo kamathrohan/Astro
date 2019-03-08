6ac10cd9fce3811cea7536f0d1e0d53ac38d7bc5
Not Unknown <rohan.kamath9@gmail.com>
2019-03-08 10:31:21 +0000

    edge masking code written

--- astroworld.py
+++ astroworld.py
@@ -14,23 +14,26 @@ plt.plot(hist)
 plt.show()
 #image_info = hdulist.info()
 
-"""
 
-img = cv2.imread("galaxies.jpeg")
 
-print(img)
+
+#print(image)
+
+
+#img = cv2.imread("galaxies.jpeg")
+
+#print(img)
 kernel = (1/25)*np.array([[0,0,0,0,0],[0,0,0,0,0],[5,5,5,5,5],[0,0,0,0,0],[0,0,0,0,0]])
 dst = cv2.GaussianBlur(img, (5,5),0)
-dst = cv2.filter2D(img,-1,kernel)
+dst = cv2.filter2D(image,-1,kernel)
 
 
 
 edges = cv2.Canny(dst,0,5)
 
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.subplot(121),plt.imshow(image,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(edges,cmap = 'gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
 plt.show()
-"""
---
+++ edgemasking.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+
+
+
+def edgemasking(data,xstart,xend,ystart,yend):
+    """
+    :param data: image whose edges need to be masked
+    :param xstart: starting value of edges (x axis)
+    :param xend:  last value of edges (x axis)
+    :param ystart: starting values of edges (y axis)
+    :param yend: last value of edges (x axis)
+    :return: masked image
+    """
+    for i in range(xstart,xend):
+        for j in range(ystart,yend):
+            data[i][j] = 0
+    return
+
+
+hdulist = fits.open("A1_mosaic.fits")
+image = hdulist[0].data
+print(np.shape(image))
+edgemasking(image,0,4611,0,100)
+edgemasking(image,0,4611,2470,2570)
+edgemasking(image,4511,4611,0,2570)
+
+#edgemasking(image,)
+plt.imshow(image)
+plt.show()
