12537c3b72c5104f2d9298d99dc5c092a2f15991
Rohan Kamath <rohan.kamath9@gmail.com>
2019-03-21 14:58:06 +0000

    object detection written for the entire sky

--- crap.py
+++ crap.py
@@ -6,8 +6,24 @@ import scipy.ndimage.morphology as sp
 
 hdulist = fits.open("A1_mosaic.fits")
 image = hdulist[0].data
+"""
+fluxmaster = []
+for i in range(100,1500,200):
+    imageslice = image[100:300, i:i+200]
+    fluxarray = em.fluxarray(imageslice, Rohan=True, im_show=True)
+    for j in fluxarray:
+        fluxmaster.append(j)
+print(fluxmaster)
 
-imageslice = image[100:500,200:400]
+"""
+
+
+
+imageslice = image[100:1000,100:1000]
 plt.imshow(imageslice)
 plt.show()
 print(em.fluxarray(imageslice, Rohan = True,im_show=True))
+
+
+
+
--- histogram.py
+++ histogram.py
@@ -7,15 +7,17 @@ import scipy.ndimage.morphology as sp
 hdulist = fits.open("A1_mosaic.fits")
 image = hdulist[0].data
 
-
+newimage = em.edgemasking(image,0,4611,0,100)
+newimage = em.edgemasking(newimage,0,4611,2470,2570)
+newimage = em.edgemasking(newimage,4511,4611,0,2570)
 
 
 numbers = []
-for i in image:
+for i in newimage:
     for j in i:
         numbers.append(j)
 
-plt.hist(numbers, bins=range(0, max(numbers) + 1000, 1000))
+plt.hist(numbers, bins=range(0, max(numbers) + 5, 5))
 plt.xlabel("Pixel Value")
 plt.ylabel("Number of Pixels")
 plt.yscale('log')
