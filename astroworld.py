68ec52dd83824a0f3e4415054ebf6242b7eb4201
Rohan Kamath <rohan.kamath9@gmail.com>
2019-04-13 13:23:35 +0100

    code reorganised

--- astroworld.py
+++ astroworld.py
@@ -23,17 +23,12 @@ newimage = masking(newimage,0,0,0,4611,2470,2570)
 newimage = masking(newimage,0,0,4511,4611,0,2570)
 
 #data split by regions by interest -->maybe need sum mo
-region_1 = newimage[2900:3500, 1200:1700] #main star and main star diffraction
-region_2 = newimage[0:4611, 1150:1600] #bleeding line from main star
-region_3 = newimage[2100:2450,600:1100] #stars region 1, left under main star
-region_4 = newimage[3000:3900, 2100:2400] #stars region 2
-region_5 = newimage[3150:3450, 600:900]
+ #main star and main star diffraction
+ #bleeding line from main star
+ #stars region 1, left under main star
+ #stars region 2
 
-edges_1 = sourcedetection(region_1)
-edges_2 = sourcedetection(region_2)
-edges_3 = sourcedetection(region_3)
-edges_4 = sourcedetection(region_4)
-edges_5 = sourcedetection(region_5)
+
 #edges_random = np.uint8(em.sourcedetection(newimage[200:400,200:400], fill = True))
 
 """
@@ -41,7 +36,7 @@ edges_5 = sourcedetection(region_5)
 Preprocessing: Masking Region 1 (main star and main star diffraction)
 ==========================================================================================================
 """
-
+region_1 = newimage[2900:3500, 1200:1700]
 edges_1 = sourcedetection(region_1)
 smooth = 255*sp.binary_fill_holes(edges_1, structure=np.ones((3,3))).astype(int)
 smooth_region1 = np.uint8(smooth)
@@ -53,6 +48,8 @@ mask_1 = masking(newimage,2900, 1200, rsy, rey, rsx, rex, a_lot  = True)
 Preprocessing: Masking Region 2 (bleeding line from main star)
 ==========================================================================================================
 """
+
+region_2 = newimage[0:4611, 1150:1600]
 edges_2 = sourcedetection(region_2)
 smooth = 255*sp.binary_fill_holes(edges_2, structure = np.ones((2,2))).astype(int)
 smooth_region2 = np.uint8(smooth)
@@ -65,6 +62,7 @@ mask_2 = masking(mask_1, 0, 1150, rsy, rey, rsx, rex, a_lot  = True)
 Preprocessing: Masking Region 3 (stars region 1, left under main star)
 ==========================================================================================================
 """
+region_3 = newimage[2100:2450,600:1100]
 edges_3 = sourcedetection(region_3)
 smooth = 255*sp.binary_fill_holes(edges_3, structure=np.ones((2,2))).astype(int)
 smooth_region3 = np.uint8(smooth)
@@ -78,6 +76,7 @@ mask_3 = masking(mask_2, 2100, 600, rsy, rey, rsx, rex, a_lot  = True)
 Preprocessing: Masking Region 4
 ==========================================================================================================
 """
+region_4 = newimage[3000:3900, 2100:2400]
 edges_4 = sourcedetection(region_4)
 smooth = 255*sp.binary_fill_holes(edges_4, structure =np.ones((2,2))).astype(int)
 smooth_region4 = np.uint8(smooth)
@@ -91,7 +90,8 @@ mask_4 = masking(mask_3, 3000, 2100, rsy, rey, rsx, rex, a_lot = True)
 Preprocessing: Masking Region 5
 ==========================================================================================================
 """
-
+region_5 = newimage[3150:3450, 600:900]
+edges_5 = sourcedetection(region_5)
 smooth = 255*sp.binary_fill_holes(edges_5, structure =np.ones((2,2))).astype(int)
 smooth_region5 = np.uint8(smooth)
 rsx,rex,rsy,rey = contour_coordinates(smooth_region5, all = False, im_show= True)
