257c002cca8b30e1bbdbb4509db8e9744c46480e
jguzmancampoy <jguzmancampoy@gmail.com>
2019-04-14 13:37:08 +0200

    changed structure again

--- astroworld.py
+++ astroworld.py
@@ -11,7 +11,6 @@ np.set_printoptions(threshold=np.nan)
 
 # Le file avec le image
 hdulist = fits.open("A1_mosaic.fits")
-
 image = hdulist[0].data
 magzpt = hdulist[0].header['MAGZPT']
 magzrr = hdulist[0].header ['MAGZRR']
@@ -22,49 +21,14 @@ newimage = masking(image,0,0,0,4611,0,100)
 newimage = masking(newimage,0,0,0,4611,2470,2570)
 newimage = masking(newimage,0,0,4511,4611,0,2570)
 
-#data split by regions by interest -->maybe need sum mo
-<<<<<<< HEAD
-region_1 = newimage[3150:3300, 1350:1550] #main star and main star diffraction
-region_2 = newimage[0:4611, 1150:1600] #bleeding line from main star
-region_3 = newimage[2100:2450, 600:1100] #stars region 1, left under main star
-region_4 = newimage[3000:3900, 2100:2400] #stars region 2
-region_5 = newimage[3150:3450, 600:900]
-region_6 = newimage[3369:3650, 1420:1450]
-region_7 = newimage[2700:2840, 950:990]
-plt.imshow(region_7)
-plt.show()
-
-edges_1 = sourcedetection(region_1)
-edges_2 = sourcedetection(region_2)
-edges_3 = sourcedetection(region_3)
-edges_4 = sourcedetection(region_4)
-edges_5 = sourcedetection(region_5)
-edges_6 = sourcedetection(region_6)
-edges_7 = sourcedetection(region_7)
-=======
- #main star and main star diffraction
- #bleeding line from main star
- #stars region 1, left under main star
- #stars region 2
-
-
->>>>>>> 68ec52dd83824a0f3e4415054ebf6242b7eb4201
-#edges_random = np.uint8(em.sourcedetection(newimage[200:400,200:400], fill = True))
-
 """
 ==========================================================================================================
 Preprocessing: Masking Region 1 (main star and main star diffraction)
 ==========================================================================================================
 """
-<<<<<<< HEAD
-
-
-smooth = 255*sp.binary_fill_holes(edges_1, structure = np.ones((3,3))).astype(int)
-=======
-region_1 = newimage[2900:3500, 1200:1700]
+region_1 = newimage[3150:3300, 1350:1550]
 edges_1 = sourcedetection(region_1)
-smooth = 255*sp.binary_fill_holes(edges_1, structure=np.ones((3,3))).astype(int)
->>>>>>> 68ec52dd83824a0f3e4415054ebf6242b7eb4201
+smooth = 255*sp.binary_fill_holes(edges_1, structure = np.ones((3,3))).astype(int)
 smooth_region1 = np.uint8(smooth)
 rsx, rex, rsy, rey = contour_coordinates(smooth_region1, all = True, im_show = False)
 mask_1 = masking(newimage, 3150, 1350, rsy, rey, rsx, rex, a_lot  = True)
@@ -88,14 +52,9 @@ mask_2 = masking(mask_1, 0, 1150, rsy, rey, rsx, rex, a_lot  = True)
 Preprocessing: Masking Region 3 (stars region 1, left under main star)
 ==========================================================================================================
 """
-<<<<<<< HEAD
-
-smooth = 255*sp.binary_fill_holes(edges_3, structure = np.ones((2,2))).astype(int)
-=======
-region_3 = newimage[2100:2450,600:1100]
+region_3 = newimage[2100:2450, 600:1100]
 edges_3 = sourcedetection(region_3)
-smooth = 255*sp.binary_fill_holes(edges_3, structure=np.ones((2,2))).astype(int)
->>>>>>> 68ec52dd83824a0f3e4415054ebf6242b7eb4201
+smooth = 255*sp.binary_fill_holes(edges_3, structure = np.ones((2,2))).astype(int)
 smooth_region3 = np.uint8(smooth)
 rsx,rex,rsy,rey = contour_coordinates(smooth_region3, all = True, im_show= False)
 mask_3 = masking(mask_2, 2100, 600, rsy, rey, rsx, rex, a_lot  = True)
@@ -107,14 +66,9 @@ mask_3 = masking(mask_2, 2100, 600, rsy, rey, rsx, rex, a_lot  = True)
 Preprocessing: Masking Region 4
 ==========================================================================================================
 """
-<<<<<<< HEAD
-
-smooth = 255*sp.binary_fill_holes(edges_4, structure = np.ones((2,2))).astype(int)
-=======
 region_4 = newimage[3000:3900, 2100:2400]
 edges_4 = sourcedetection(region_4)
-smooth = 255*sp.binary_fill_holes(edges_4, structure =np.ones((2,2))).astype(int)
->>>>>>> 68ec52dd83824a0f3e4415054ebf6242b7eb4201
+smooth = 255*sp.binary_fill_holes(edges_4, structure = np.ones((2,2))).astype(int)
 smooth_region4 = np.uint8(smooth)
 rsx,rex,rsy,rey = contour_coordinates(smooth_region4, all = True, im_show= False)
 mask_4 = masking(mask_3, 3000, 2100, rsy, rey, rsx, rex, a_lot = True)
@@ -126,14 +80,9 @@ mask_4 = masking(mask_3, 3000, 2100, rsy, rey, rsx, rex, a_lot = True)
 Preprocessing: Masking Region 5
 ==========================================================================================================
 """
-<<<<<<< HEAD
-
-smooth = 255*sp.binary_fill_holes(edges_5, structure = np.ones((2,2))).astype(int)
-=======
 region_5 = newimage[3150:3450, 600:900]
 edges_5 = sourcedetection(region_5)
-smooth = 255*sp.binary_fill_holes(edges_5, structure =np.ones((2,2))).astype(int)
->>>>>>> 68ec52dd83824a0f3e4415054ebf6242b7eb4201
+smooth = 255*sp.binary_fill_holes(edges_5, structure = np.ones((2,2))).astype(int)
 smooth_region5 = np.uint8(smooth)
 rsx,rex,rsy,rey = contour_coordinates(smooth_region5, all = True, im_show = False)
 mask_5 = masking(mask_4, 3150, 600, rsy, rey, rsx, rex, a_lot = True)
@@ -143,7 +92,8 @@ mask_5 = masking(mask_4, 3150, 600, rsy, rey, rsx, rex, a_lot = True)
 Preprocessing: Masking Region 6
 ==========================================================================================================
 """
-
+region_6 = newimage[3369:3650, 1420:1450]
+edges_6 = sourcedetection(region_6)
 smooth = 255*sp.binary_fill_holes(edges_6, structure = np.ones((3,3))).astype(int)
 smooth_region6 = np.uint8(smooth)
 rsx,rex,rsy,rey = contour_coordinates(smooth_region5, all = False, im_show= False)
@@ -156,7 +106,8 @@ mask_6 = masking(mask_5, 3369, 1420, rsy, rey, rsx, rex, a_lot = True)
 Preprocessing: Masking Region 7
 ==========================================================================================================
 """
-
+region_7 = newimage[2700:2840, 950:990]
+edges_7 = sourcedetection(region_7)
 smooth = 255*sp.binary_fill_holes(edges_7, structure = np.ones((3,3))).astype(int)
 smooth_region7 = np.uint8(smooth)
 rsx,rex,rsy,rey = contour_coordinates(smooth_region5, all = True, im_show= False)
@@ -169,11 +120,7 @@ fig, (ax1, ax2) = plt.subplots(1,2)
 
 ax1.imshow(newimage)
 ax1.set_title('Before Masking')
-<<<<<<< HEAD
 ax2.imshow(mask_7)
-=======
-ax2.imshow(mask_4)
->>>>>>> 68ec52dd83824a0f3e4415054ebf6242b7eb4201
 ax2.set_title('After Masking')
 plt.show()
 
